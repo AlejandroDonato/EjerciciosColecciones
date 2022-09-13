20acc8a4bff4010d80a13686cf758d5986c937a2
AlejandroDonato <86857154+AlejandroDonato@users.noreply.github.com>
2022-09-12 21:57:28 -0300

    primer commit

---
+++ ejercicio01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            List<int> numeros = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                numeros.Add(random.Next(-100, 100));
+            }
+            foreach (int UnNumero in numeros) 
+            {
+                Console.WriteLine(UnNumero);
+            }
+            Console.WriteLine();
+
+            numeros.Sort();
+
+            foreach (int numOrden in numeros)
+            {
+                Console.WriteLine(numOrden);
+            }
+
+            if (numeros.Count() > 0)
+            { 
+                numeros.Reverse();  
+            }
+
+            Console.WriteLine("Prdenados mayor a menor");
+
+            foreach (int numOrden in numeros)
+            {
+                Console.WriteLine(numOrden);
+            }
+        }
+    }
+}
---
+++ ejercicio02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ejercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack <int> numeros = new Stack <int>();
+            List <int> auxNums = new List <int>();
+            Random random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                numeros.Push(random.Next(-100,100));
+            }
+
+            Console.WriteLine("lista inicial");
+
+            foreach (int num in numeros) 
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("lista ordenada 1");
+            auxNums = numeros.ToList();
+            auxNums.Sort();
+            numeros = new Stack <int>(auxNums);
+
+            foreach (int num in numeros)
+            {
+                if (num > 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
+            Console.WriteLine("lista ordenada 2");
+            auxNums.Reverse();
+            numeros = new Stack<int>(auxNums);
+
+            foreach (int num in numeros)
+            {
+                if (num > 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
